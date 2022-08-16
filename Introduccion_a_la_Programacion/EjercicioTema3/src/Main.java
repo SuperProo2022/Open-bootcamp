472a36e3866d63fd10c03a3d1879c7180fd19cbf
SuperProo2022 <Adriel.SuperProo@gmail.com>
2022-08-16 13:12:38 -0300

    Introduccion_Programacion
    Ejerciciotema3

---
+++ Introduccion_a_la_Programacion/EjercicioTema3/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        //Primera parte:
+        //Llamar a la función en el main y darle valores.
+        sumarymostrar(3,4,5); //3+4+5=12
+        System.out.println(sumarydevolver(3,4,5));//3+4+5=12
+        //Segunda parte:
+        //Crear un objeto miCoche en el main y añadirle una puerta.
+        //Mostrar el número de puertas que tiene el objeto.
+        Coche miCoche = new Coche();
+        miCoche.IncrementarPuertas();
+        System.out.println(miCoche.Puertas);
+    }
+    //Primera parte:
+    //
+    //Crear una función con tres parámetros que sean números que se suman entre sí.
+    public static void sumarymostrar(int valor1, int valor2, int valor3) {
+
+        System.out.println(valor1 + valor2 + valor3); // Muestra por pantalla la suma de los 3 valores
+
+    }
+    public  static  int sumarydevolver(int valor1,int valor2,int valor3){
+
+        return valor1+valor2+valor3; //Devuelve el valor de la suma de los 3 valores
+    }
+}
+//Segunda parte:
+//Crear una clase coche.
+//Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+//Una función que incremente el número de puertas que tiene el coche.
+class Coche {
+    public int Puertas = 0;
+    public void IncrementarPuertas(){
+        this.Puertas++;
+    }
+}
