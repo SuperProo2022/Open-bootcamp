94cb65a7d2264420c16b64d33444529651c726f4
SuperProo2022 <111371889+SuperProo2022@users.noreply.github.com>
2022-08-22 01:31:00 -0300

    EjercicioTema8
    OpenBootcamp: Practica de la encapsulación.

---
+++ Introduccion_a_la_Programacion/EjercicioTema8/src/Main.java
@@ -0,0 +1,39 @@
+//Para practicar la encapsulación:
+//Crear clase Persona.
+//Crear variables las privadas edad, nombre y telefono de la clase Persona.
+//Crear gets y sets de cada propiedad.
+//Crear un objeto persona en el main.
+//Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono,
+// por último muéstralas por consola.
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setEdad(20);
+        persona.setNombre("Horacio");
+        persona.setTelefono("3804252530");
+        System.out.println("El nombre de la persona es : "+ persona.getNombre()+"\nSu edad es : "+persona.getEdad()+"\nSu telefono es : "+persona.getTelefono());
+    }
+    }
+    class Persona{
+    private int edad;
+    private String nombre;
+    private  String telefono;
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+        public int getEdad() {
+            return edad;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public String getNombre() {
+            return nombre;
+        }
+        public void setTelefono(String telefono) {
+            this.telefono = telefono;
+        }
+        public String getTelefono() {
+            return telefono;
+        }
+    }
