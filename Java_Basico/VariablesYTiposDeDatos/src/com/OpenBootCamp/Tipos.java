51ba2b8edea9abfa67e2ad257752b09e1d9f9b57
SuperProo2022 <111371889+SuperProo2022@users.noreply.github.com>
2022-09-06 01:43:26 -0300

    Add files via upload

---
+++ Java_Basico/VariablesYTiposDeDatos/src/com/OpenBootCamp/Tipos.java
@@ -0,0 +1,32 @@
+package com.OpenBootCamp;
+
+public class Tipos {
+    public static void main(String[] args) {
+
+
+        //Enteros
+        byte number1=1;
+        short number2=2;
+        int number3=3;
+        long number4=4;
+
+        //Flotante
+        float decimal1=4.9f;
+        double decimal2=9.99d;
+
+        //Caracter
+        char caracter1='a';
+
+        //Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //Cadenas de Texto
+        String nombre="Pedro";
+        String apellido="Satiro";
+
+        //Tipos Envoltorios
+        Integer numero = null;
+        Long numero2=2L;
+    }
+}
