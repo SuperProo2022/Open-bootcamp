2c8dc9d6d8093c22f05ff30139de1fb844bcb5ef
SuperProo2022 <111371889+SuperProo2022@users.noreply.github.com>
2022-09-06 01:54:19 -0300

    Add files via upload

---
+++ Java_Basico/Operadores/src/com/OpenBootCamp/Operadores.java
@@ -0,0 +1,55 @@
+package com.OpenBootCamp;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        //Operadores
+
+        //Aritmeticos
+
+        // + - / * %
+
+        int number1= 1;
+        int number2= 2;
+
+        int resultado1 = number1+number2;
+        int resultado2 = number1-number2;
+        int resultado3 = number1*number2;
+        int resultado4 = number1/number2;
+
+        //Logicos , Relacion , Comparacion , Booleanos
+
+        /*
+        > Mayor que
+        >= Mayor igual que
+        <= Menor igual que
+        < Menor que
+        == Igual a
+        != Distinto
+        && Y And
+        || O Or
+        ! Negacion
+        */
+
+        //Asignacion
+
+        /*
+        = igual
+        +=
+        -=
+        /=
+        *=
+        %=
+         */
+        //Incremento
+        //++
+        //Decremento
+        //--
+
+
+        //Concatenacion
+        //+
+
+        
+    }
+}
